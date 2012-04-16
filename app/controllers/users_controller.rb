b8690b75fa1e746f0f42ac0762f3568bea7800f0
Tal Keinan <tal.keinan@gmail.com>
2012-04-16 13:41:49 -0700

    Added Signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
   def new
   	@title = "Sign up"
+  	@user = User.new
   end
 
   def show
   	@user = User.find(params[:id])
   	@title = User.find(params[:id]).name
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	@title = "Create"
+  	if @user.save
+  		render 'show'
+  	else
+  		render 'new'
+  	end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,7 +7,7 @@ describe "UserPages" do
 	describe "Signup page" do
     	before { visit signup_path }
 
-    	it { should have_selector 'h1', text: 'Signup' }
+    	it { should have_selector 'h1', text: 'Sign up' }
     	it { should have_selector 'title', text: full_title('Sign up') }
     end
 
@@ -17,5 +17,27 @@ describe "UserPages" do
 
     	it { should have_selector 'h1', text: user.name }
     	it { should have_selector 'title', text: user.name }
+    end
+
+    describe "signup" do
+        before { visit signup_path }
+        describe "with invalid information" do
+            it "should not create a user" do
+                expect { click_button "Create my account" }.not_to change(User, :count)
+            end
+        end
+
+        describe "with valid infomration" do
+            before do
+                fill_in "Name",         with: "Example User"
+                fill_in "Email",        with: "user@example.com"
+                fill_in "Password",     with: "foobar"
+                fill_in "Confirmation", with: "foobar"
+            end
+
+            it "should create a user" do
+                expect { click_button "Create my account" }.to change(User, :count)
+            end
+        end
     end
 end
