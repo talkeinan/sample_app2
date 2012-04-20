72d87afdb1425db32408784b96ee5dd288c24a8e
Tal Keinan <tal.keinan@gmail.com>
2012-04-19 18:55:18 -0700

    Created Sign_in Form.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,17 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		@title = 'Create'
+		user = User.find_by_email(params[:session][:email])
+		if user && user.authenticate(params[:session][:password])
+			sign_in user
+			redirect_to user
+			# Sign in the user and redirect to user's show page
+		else
+			flash.now[:error] = "Invalid email/password combination"
+			render 'new'
+			# Create an error message and re-render the signin form
+		end
 	end
 
 	def destroy
