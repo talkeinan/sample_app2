db93327c1ab48982e598fd451cc1dace06ef3c72
Tal Keinan <tal.keinan@gmail.com>
2012-04-20 16:16:51 -0700

    Added Edit form for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,4 +20,22 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit
+  	@title = "Edit user"
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  	@title = "Update user"
+  	@user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,11 +25,12 @@ describe "Authentication" do
 
   		it { should have_selector('title', text: user.name) }
   		it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
   		it { should have_link('Sign out', href: signout_path) }
   		it { should_not have_link('Sign in', href: signin_path) }
 
   		describe "followed by Signout" do
-  			before { click_link "sign out" }
+  			before { click_link "Sign out" }
   			it { should have_link('Sign in') }
   		end
   	end
