28f660af53370634deee8917c0637d48a0fb1eed
Tal Keinan <tal.keinan@gmail.com>
2012-04-29 18:49:06 -0700

    Completed Microposts / User Associations
    Signed-off-by: Tal Keinan <tal.keinan@gmail.com>

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ActiveRecord::Base
 	attr_accessible :content
-	belongs_to :user
-	validates :user_id, presence: true
+	belongs_to 		:user
+	validates 		:user_id, presence: true
+	default_scope 	order: 'microposts.created_at DESC'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,12 +28,35 @@ describe User do
 	it { should be_valid }
 
 
+  describe "micropost associations" do
+    before { @user.save }
+    let!(:older_micropost) do 
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts
+      @user.destroy
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+  end
+
+
   it { should_not be_admin }
   describe "with admin attribute set to 'true'" do
     before { @user.toggle!(:admin) }
     it { should be_admin }
   end
-
 
   describe "remember token" do
     before { @user.save }
@@ -41,63 +64,62 @@ describe User do
   end
 
 	describe "when name is not present" do
-    	before { @user.name = "" }
-    	it { should_not be_valid }
-  	end
+    before { @user.name = "" }
+    it { should_not be_valid }
+  end
 
-  	describe "when email is not present" do
+  describe "when email is not present" do
 		before { @user.email = "" }  		
 		it { should_not be_valid }
-  	end
+  end
 
-  	describe "when name is too long" do
-  		before { @user.name = 'a' * 51 }
+  describe "when name is too long" do
+  	before { @user.name = 'a' * 51 }
 		it { should_not be_valid } 
-  	end
+  end
 
-  	describe "when email format is invalid" do
-    	invalid_addresses =  %w[user@foo,com user_at_foo.org example.user@foo.]
-    	invalid_addresses.each do |invalid_address|
-      	before { @user.email = invalid_address }
-      		it { should_not be_valid }
-    	end	
-  	end
+  describe "when email format is invalid" do
+    invalid_addresses =  %w[user@foo,com user_at_foo.org example.user@foo.]
+    invalid_addresses.each do |invalid_address|
+      before { @user.email = invalid_address }
+      it { should_not be_valid }
+    end	
+  end
 
-  	describe "when email format is valid" do
-    	valid_addresses = %w[user@foo.com A_USER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-    	valid_addresses.each do |valid_address|
-      	before { @user.email = valid_address }
-      		it { should be_valid }
-    	end
-  	end
+  describe "when email format is valid" do
+    valid_addresses = %w[user@foo.com A_USER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+    valid_addresses.each do |valid_address|
+      before { @user.email = valid_address }
+      it { should be_valid }
+    end
+  end
 
-  	describe "when email address is already taken " do
-  		before do
-  			duplicate_user = @user.dup
-  			duplicate_user.email = @user.email.upcase
-  			duplicate_user.save
-  		end
-  		it { should_not be_valid }
+  describe "when email address is already taken " do
+  	before do
+  		duplicate_user = @user.dup
+  		duplicate_user.email = @user.email.upcase
+  		duplicate_user.save
   	end
+  	it { should_not be_valid }
+  end
 	
 	describe "when password is not present" do
-  		before { @user.password = @user.password_confirmation = " " }
-  		it { should_not be_valid }
+  	before { @user.password = @user.password_confirmation = " " }
+  	it { should_not be_valid }
 	end
 
 	describe "when password doesn't match confirmation" do
-  		before { @user.password_confirmation = "mismatch" }
-  		it { should_not be_valid }
+  	before { @user.password_confirmation = "mismatch" }
+  	it { should_not be_valid }
 	end
 
 	describe "when password confirmation is nil" do
-  		before { @user.password_confirmation = nil }
-  		it { should_not be_valid }
+  	before { @user.password_confirmation = nil }
+  	it { should_not be_valid }
 	end
 
-  	describe "with a password that's too short" do
-    	before { @user.password = @user.password_confirmation = "a" * 5 }
-    	it { should be_invalid }
-  	end
-
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
 end
